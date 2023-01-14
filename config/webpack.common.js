e714cf43c3a5d31160a537ff89f1dfdeea764759
vupham <vuquangpham2909@gmail.com>
2023-01-14 22:46:36 +0700

    :construction: webpack config > change the file type of file-loader (font)

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -30,13 +30,11 @@ module.exports = {
         rules: [
 
             {
-                test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
-                loader: 'file-loader',
-                options: {
-                    name(file){
-                        return '[hash].[ext]';
-                    }
-                }
+                test: /\.(png|jpg|gif|jpe?g|svg|woff2?|fnt|webp|mp4)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: '[hash].[ext]',
+                },
             },
         ]
     },
